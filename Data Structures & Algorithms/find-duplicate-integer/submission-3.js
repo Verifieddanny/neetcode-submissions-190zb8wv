0754e318229e0f0cb3d74839095d275240f45305
Daniel Nwachukwu <dannyclassic56@gmail.com>
2026-04-01 10:27:44 +0100

    Add: find-duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number}
+   */
+  findDuplicate(nums) {
+    let set = new Set();
+    let l = 0;
+    let r = nums.length - 1;
+
+    while (l <= r) {
+      if (nums[l] !== nums[r]) {
+        if (!set.has(nums[l]) && !set.has(nums[r])) {
+          set.add(nums[l]);
+          set.add(nums[r]);
+          l++;
+          r--;
+        } else if (set.has(nums[l])) {
+          return nums[l]
+        } else if (set.has(nums[r])) {
+          return nums[r]
+        }
+      } else if (l === r && set.has(nums[l])) {
+        return nums[l]
+      } else {
+        return nums[l]
+      }
+    }
+    return 0
+  }
+}
