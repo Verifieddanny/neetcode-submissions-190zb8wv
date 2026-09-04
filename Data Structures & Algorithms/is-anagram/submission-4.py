037196a7775bfe6be177213825f77859bf15246b
Daniel Nwachukwu <danielnwachukwuchigozirim@gmail.com>
2026-09-04 11:52:59 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False
+        
+        length = len(s)
+        s_hash_map = {}
+        t_hash_map = {}
+
+        for i in range(length):
+            s_hash_map[s[i]] = s_hash_map.get(s[i], 0) + 1
+            t_hash_map[t[i]] = t_hash_map.get(t[i], 0) + 1
+        
+        for idx, key in enumerate(s_hash_map):
+            t_val = t_hash_map.get(key, 0)
+            if(s_hash_map[key] != t_val):
+                return False
+            
+        return True
+        
