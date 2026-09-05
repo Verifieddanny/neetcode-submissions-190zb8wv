439dae79479ed5171f1cc2b0bc02dca606d07b5e
Daniel Nwachukwu <danielnwachukwuchigozirim@gmail.com>
2026-09-05 20:27:35 +0100

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if(len(s) != len(t)):
+            return False
+        
+        count = len(s)
+        s_hash_map = {}
+        t_hash_map = {}
+
+        for i in range(count):
+            s_hash_map[s[i]] = s_hash_map.get(s[i], 0) + 1
+            t_hash_map[t[i]] = t_hash_map.get(t[i], 0) + 1
+
+        return s_hash_map == t_hash_map
+        
